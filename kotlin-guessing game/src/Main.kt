ea0826f137ee70e0e671b108a69c22773ad2c9a2
Chenitha0823 <chenitharithmal@gmail.com>
2025-02-11 15:09:42 +1300

    Start the new project

---
+++ kotlin-guessing game/src/Main.kt
@@ -0,0 +1,26 @@
+/**
+ * A LITTLE GUESSING GAME IN KOTLIN
+ */
+
+fun main() {
+    println("_____________")
+    println("guessing game 5 million")
+    println("______________")
+    println()
+
+    val numToGuess = (1..100). random()
+
+    println("I'm thinking of a number between 1 and 100)")
+    println("can you guess it?")
+
+    //Main loop
+    while (ture) {
+        // Ask the user for a guess
+        // check if it's correct or not
+        // Break out of loop of correct
+        // otherwise give high or lower feedback
+    }
+
+
+
+}
