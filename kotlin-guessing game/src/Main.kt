833c29113329b9d673f512247779bd3feafaad37
Chenitha0823 <chenitharithmal@gmail.com>
2025-02-17 11:01:17 +1300

    New update

--- kotlin-guessing game/src/Main.kt
+++ kotlin-guessing game/src/Main.kt
@@ -8,6 +8,8 @@ fun main() {
     println("______________")
     println()
 
+    val playername = getString(prompt:"Enter your name"
+
     val numToGuess = (1..100). random()
 
     println("I'm thinking of a number between 1 and 100)")
@@ -21,6 +23,35 @@ fun main() {
         // otherwise give high or lower feedback
     }
 
+    /**
+     * Function to get a string from the user
+     * Parameters:
+     * Returns
+     * -string that the user types
+     */
+    Fun getstirng(prompt:String):String {
+
+         while (true){
+             print(prompt)
+
+             val userInput = readLn()
+             if (userInput.isNotBlank()) break
+         }
+
+        return userInput
+    }
+
+    /** finction to get an Int from the user
+     *
+     */
+    fun getInt(prompt: String) : Int{
+        var intValue : Int
+
+        while(true)
+        val userInput = getSring(prompt)
+        intValue = userInput.toIntOrNull()
+        if (intValue != null ) break
+    }
 
 
 }
